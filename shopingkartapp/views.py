26e4c5ee2f53095bc6b0f22f83747ccf8aa757d8
Vishalgithub0987 <vishalsangeeth71@gmail.com>
2025-03-17 22:30:22 +0530

    completed with mobile models

--- shopingkartapp/views.py
+++ shopingkartapp/views.py
@@ -18,11 +18,23 @@ def collections(request):
 def collectionsview(request,name):
     if(Category.objects.filter(name=name,status=0)):
         products = Product.objects.filter(Category__name=name)
-        return render(request, "shop/products/index.html",{'products':products})
+        return render(request, "shop/products/index.html",{'products':products,"category_name":name}) 
     else:
         messages.warning(request, 'Category not found')
         return redirect('collections')
-
+    
+def product_details(request,cname,pname):
+    if(Category.objects.filter(name=cname,status=0)):
+        if(Product.objects.filter(name=pname,status=0)):
+            products = Product.objects.filter(name=pname,status=0).first()
+            return render(request, "shop/products/product_details.html",{'products':products}) 
+        else:
+            messages.error(request, 'Product not found')
+            return redirect('collections')
+    
+    else:
+        messages.error(request, 'Category not found')
+        return redirect('collections')
  
 
 
